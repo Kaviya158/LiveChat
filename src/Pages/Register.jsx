b624bd4a76c20d4fda6aec0ed591ca556572b7f1
KaviyaShanmugaSundaram <Kaviya.ShanmugaSundaram@alcumus.com>
2024-10-28 20:18:21 +0530

    Completed UI and validation part of Registration page

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,20 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import "./Register.css";
 import logo from "../assests/logo.svg";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "./toaststyle.css";
+import axios from "axios";
+import { registerRoute } from "../utils/APIRoute";
 
 const Register = () => {
-  const handleSubmit = (e) => {
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmpassword: "",
+  });
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    alert("Form Submitted");
+    if (handleValidation()) {
+      const { username, email, password, confirmpassword } = values; // Ensure variable name is correct
+      const { data } = await axios.post(registerRoute, {
+        username,
+        email,
+        password,
+      });
+    }
   };
-  const handleChange = () => {};
+  const handleChange = (event) => {
+    setValues({ ...values, [event.target.name]: event.target.value });
+  };
+  const toastOptions = {
+    position: "bottom-right",
+    autoClose: 8000,
+    pauseOnHover: true,
+    draggable: true,
+  };
+  const handleValidation = () => {
+    const { username, email, password, confirmpassword } = values; // Ensure variable name is correct
+
+    if (!username && !email && !password && !confirmpassword) {
+      toast.error("All fields are required.", {
+        toastOptions,
+      });
+    } else {
+      if (username.length < 3) {
+        toast.error("Username should be greater than 3", { toastOptions });
+      }
+
+      if (password.length < 8) {
+        toast.error("Password should be greater than 8", { toastOptions });
+      }
+
+      if (password !== confirmpassword) {
+        toast.error("Password should match with confirmpassword", {
+          toastOptions,
+        });
+      }
+    }
+    return true;
+  };
+
   return (
     <div>
       <FormContainer className="FormContainer">
         <form onSubmit={(e) => handleSubmit(e)}>
           <div className="brand">
-            <img src={logo} alt="logo" className="logoDesign"/>
+            <img src={logo} alt="logo" className="logoDesign" />
             <h1 className="chatName">Negesydd</h1>
           </div>
           <input
@@ -43,17 +94,15 @@ const Register = () => {
           />
           <button type="submit">Create User</button>
           <span>
-            already have an account? <a href='/login'>Login</a>
+            already have an account? <a href="/login">Login</a>
           </span>
         </form>
       </FormContainer>
+      <ToastContainer />
     </div>
   );
 };
 
-const FormContainer = styled.div`
- 
-`;
-
+const FormContainer = styled.div``;
 
 export default Register;
